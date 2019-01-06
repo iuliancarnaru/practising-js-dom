465c94fe589301b49c46a073613ae17ab40ebf23
iuliancarnaru <iulian.carnaru@yahoo.co.uk>
2019-01-06 18:50:02 +0000

    selecting and modifying DOM

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-console.log('Connected!');
+
+let p1Score = 0;
+let p2Score = 0;
+
+const p1Button = document.querySelector('#p1');
+const p2Button = document.getElementById('p2');
+
+const p1Dispaly = document.querySelector('#p1-display');
+const p2Dispaly = document.querySelector('#p2-display');
+
+p1Button.addEventListener('click', () => {
+    p1Score++;
+    p1Dispaly.textContent = p1Score;
+})
+
+p2Button.addEventListener('click', () => {
+    p2Score++;
+    p2Dispaly.textContent = p2Score;
+})
